d80296d7e84c70c1bfda24787ce95f36222996d9
EC2 Default User <abbieneufeld@gmail.com>
2021-05-25 02:17:45 +0000

    adding Exercises: OO Basics 1 and OO Basics 2

---
+++ Exercises/OO_Basics_2/6.rb
@@ -0,0 +1,14 @@
+class Cat
+  attr_reader :name
+  COLOR="purple"
+  def initialize(name)
+    @name=name
+  end
+  
+  def greet
+    puts"Hello my name is #{name} and I'm a #{COLOR} cat!"
+  end
+end
+
+kitty = Cat.new('Sophie')
+kitty.greet
